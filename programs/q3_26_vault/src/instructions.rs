def23ad51062b2f608f79231c9e3ad49c484bbc5
Gopichand <gopichand0816@gmail.com>
2026-09-09 06:45:01 +0000

    Complete vault program with withdraw and close + tests

--- programs/q3_26_vault/src/instructions.rs
+++ programs/q3_26_vault/src/instructions.rs
@@ -1,9 +1,9 @@
 pub mod initialize;
 pub mod deposit;
-// pub mod withdraw;
-// pub mod close;
+pub mod withdraw;
+pub mod close;
 
 pub use initialize::*;
 pub use deposit::*;
-// pub use withdraw::*;
-// pub use close::*;
+pub use withdraw::*;
+pub use close::*;
---
+++ programs/q3_26_vault/src/instructions/close.rs
@@ -0,0 +1,54 @@
+use crate::{
+    constants::{STATE, VAULT_SEED},
+    state::VaultState,
+};
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [STATE, user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        mut,
+        seeds = [VAULT_SEED, user.key().as_ref()],
+        bump = vault_state.vault_bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let amount = self.vault.lamports();
+
+        let seeds = &[
+            VAULT_SEED,
+            self.user.key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_program = self.system_program.key();
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, amount)
+    }
+}
